a7921f163147240d94b9ca07bf92d95e3099fe8c
LuizaLorien <139228809+LuizaLorien@users.noreply.github.com>
2024-05-06 12:41:38 -0300

    finishing

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Container from './Container/Container';
+import './App.css'
+
+function App() {
+  return(
+  <>
+  <Container>
+   <ContainerModal TextModal={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod harum nobis eos architecto, saepe aspernatur! Magnam veniam eaque quos animi unde cum quidem perspiciatis, id deserunt neque iste error nam."} TextBtn={"What is front end mentor, and how it will help me?"}/>
+  </Container>
+  </>
+  )
+}
+
+export default App
---
+++ src/ContainerModal/ContainerModal.jsx
@@ -0,0 +1,3 @@
+import Modal from '../Modal/Modal';
+import Button from '../ButtonModal/ButtonModal';
+import React from 'react';
